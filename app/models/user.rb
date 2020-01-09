3fe6a9412556fd3dcb317b7eface4b64720ca720
koba7taku <58512543+koba7taku@users.noreply.github.com>
2020-01-09 14:42:38 +0900

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
 
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  
-  has_many :buyed_items, foreign_key: “buyer_id”, class_name: “Item” 
-  has_many :saling_items, -> { where(“buyer_id is NULL”) }, foreign_key: “saler_id”, class_name: “Item” 
-  has_many :sold_items, -> { where(“buyer_id is not NULL”) }, foreign_key: “saler_id”, class_name: “Item”
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
+
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_info, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
