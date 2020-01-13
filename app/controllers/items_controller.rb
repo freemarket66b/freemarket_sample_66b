d80a04bad8d34704455e2b70bd1df5c5eda69567
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-01-13 20:31:28 +0900

    出品機能β版

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,10 @@ end
 
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    end
   end
 
   def show
@@ -17,4 +21,11 @@ end
 
   def buy
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:explanation,:status,:delivery_type,:postage,:region,:shipping_date,:price,:category).merge(saler_id:current_user.id)
+  end
+
 end
