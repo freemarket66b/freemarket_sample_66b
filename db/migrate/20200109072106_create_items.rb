ba9c51edce35224c8a9119d879035db16f4cbdb3
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-01-14 16:08:12 +0900

    出品viewの完成（コードレビュー待ち）

--- db/migrate/20200109072106_create_items.rb
+++ db/migrate/20200109072106_create_items.rb
@@ -1,13 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :items do |t|
       t.string :name,    null: false
       t.string :explanation,    null: false, index: true
-      t.string :status,    null: false
-      t.string :delivery_type,    null: false
-      t.string :postage,    null: false
-      t.string :region,    null: false
-      t.string :shipping_date, null: false
+      t.integer :status,    null: false
+      t.integer :delivery_type,    null: false
+      t.integer :postage,    null: false
+      t.integer :region,    null: false
+      t.integer :shipping_date, null: false
       t.integer :price,    null: false
       t.string :brand
       t.references :category, foreign_key:true
@@ -15,7 +15,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
       t.references :buyer_id,    foreign_key: { to_table: :users }
       t.timestamps
 
-      remove_index
+      # remove_index :items, column: :explanation 
     end
   end
 end
