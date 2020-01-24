c079839916f0bc01a6db6fd7796fe821e2cbb092
kata-tapi <get.gb.till32@gmail.com>
2020-01-24 19:48:31 +0900

    fixed

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -45,7 +45,7 @@ class CardController < ApplicationController
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 end
